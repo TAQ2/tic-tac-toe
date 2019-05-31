1dafe1c4e9a27aadb6e37c46b09c81bea39189f8
arjung <arjungupta2110@gmail.com>
2019-05-31 13:57:07 +0100

    Updates

---
+++ precache-manifest.e45bc0e3fdfe53a5cca6788354d50493.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4839d8707e069c4dc74da2f51c446fa0",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "51fd61cf80bcc4a79630",
+    "url": "/tic-tac-toe/static/css/main.952475e9.chunk.css"
+  },
+  {
+    "revision": "412e92c1e58cc8eaa279",
+    "url": "/tic-tac-toe/static/js/2.96e3bbd7.chunk.js"
+  },
+  {
+    "revision": "51fd61cf80bcc4a79630",
+    "url": "/tic-tac-toe/static/js/main.f25fb996.chunk.js"
+  },
+  {
+    "revision": "9728fa41a930f12fdc87",
+    "url": "/tic-tac-toe/static/js/runtime~main.f1e29306.js"
+  },
+  {
+    "revision": "1e670d88b23c7ab956f1829e3828a210",
+    "url": "/tic-tac-toe/static/media/digital-clock-font.1e670d88.ttf"
+  }
+]);
