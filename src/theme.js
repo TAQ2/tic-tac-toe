039e4215382b8f3b3546cd69972284847e12d0e0
arjung <arjungupta2110@gmail.com>
2019-06-11 17:20:14 +0100

    update colour theme

--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,7 @@
-const primary = "#d8c52e";
-const secondary = "#ea634f";
+const primary = "#e2ab5b";
+const secondary = "#DEC9C5";
 const tertiary = "#483F54";
-const quaternary = "#23b314";
+const quaternary = "#7ea951";
 const quinary = "#7B6287";
 
 export default {
